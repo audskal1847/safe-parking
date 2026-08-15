4f2dff0b451c5d80dd7941cf9f083fa0c1acc1e8
audskal1847 <audskal184700@gmail.com>
2026-08-15 21:30:38 +0900

    Enable notification sound

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,6 @@ self.addEventListener('push', function (event) {
   icon: '/icon-192.png',
   badge: '/icon-192.png',
   silent: false,
-  renotify: true,
   data: {
     url: data.url || '/'
   },
